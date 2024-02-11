8775eb91e5f81bfc99de186f3f70af353296fb64
LFMarlow <122919718+LFMarlow@users.noreply.github.com>
2024-02-11 23:17:09 +0100

    Add files via upload

---
+++ Classes/Recipes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace Test_ECF.Classes
+{
+    public class Recipes
+    {
+        public String titre;
+        public String URLImage;
+        public String time;
+        public String timeRepos;
+        public String timePrepa;
+        public String description;
+        public String allergenes;
+        public String regime;
+        public String ingredients;
+        public String etapes;
+        public bool estPatient;
+
+        public Recipes()
+        {
+
+        }
+
+        public Recipes(string prmTitre, string prmURL, string prmTime, string prmTimeRepos, string prmTimePrepa ,string prmDescription, string prmAllergenes, string prmRegime, string prmIngredients, string prmEtapes, bool prmEstPatient)
+        {
+            titre = prmTitre;
+            URLImage = prmURL;
+            time = prmTime;
+            timeRepos = prmTimeRepos;
+            timePrepa = prmTimePrepa;
+            description = prmDescription;
+            allergenes = prmAllergenes;
+            regime = prmRegime;
+            ingredients = prmIngredients;
+            etapes = prmEtapes;
+            estPatient = prmEstPatient;
+        }
+    }
+}
---
+++ Classes/Users.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_ECF.Classes
+{
+    public class Users
+    {
+        public String nom;
+        public String prenom;
+        public String email;
+        public String password;
+        public String roleUsers;
+        public String Allergenes;
+
+        public Users()
+        {
+
+        }
+
+        public Users(string prmMail, string prmPassword)
+        {
+            email = prmMail;
+            password = prmPassword;
+        }
+
+        public Users(string prmNom, string prmPrenom, string prmEmail, string prmPassword, string prmRoles, string prmAllergenes)
+        {
+            nom = prmNom;
+            prenom = prmPrenom;
+            email = prmEmail;
+            password = prmPassword;
+            roleUsers = prmRoles;
+            Allergenes = prmAllergenes;
+        }
+    }
+}
